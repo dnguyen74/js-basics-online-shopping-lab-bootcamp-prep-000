e0d0d4566a459511a38fd35a75fa872da1716ad4
Dat Nguyen <dnguyen74@gmail.com>
2018-03-02 03:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,43 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  // write your code here
+  var itemPrice = Math.floor(Math.random() * 100) + 1;
+ 
+  var item = {[itemName]: itemPrice};
+  
+  cart.push([item]);
+  
+  console.log(itemName + " has been added to your cart.");
+  return cart;
 }
 
 function viewCart() {
   // write your code here
+  var cartItems = "";
+  if (cart.length === 0) {
+    cartItems = "Your shopping cart is empty.";
+  }
+  else if (cart.length ===  1){
+    cartItems = "In your cart, you have " + 
+    cart[0] + " at $" + cart[0][0] +".";
+  }
+  else {
+    cartItems = "In your cart, you have ";
+    for (let i = 0; i < cart.length; i++) {
+      cartItems = cartItems + cart[i] + " at " + cart[i] + ".";
+    }
+  }
+  console.log(cartItems);
 }
 
 function total() {
   // write your code here
+  var cartValue = 0;
+  for (let i=0; i<cart.length; i++) {
+    cartValue = cartValue + cart[i];
+  }
 }
 
 function removeFromCart(item) {
